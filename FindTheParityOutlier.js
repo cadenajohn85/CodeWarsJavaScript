03c9e32f28ba5e798177583d910f131f15b83245
John Cadena <cadena.john.85@gmail.com>
2019-12-23 16:23:38 -0600

    refactor FindTheParityOutlier based on community best practices solutions

--- FindTheParityOutlier.js
+++ FindTheParityOutlier.js
@@ -2,6 +2,8 @@
 // array is either entirely comprised of odd integers or entirely comprised of even integers except for a single
 // integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
+
+// Original Solution
 function findOutlier(integers){
     let oddCounter = 0, evenCounter = 0;
     let isMostlyOdd;
@@ -32,5 +34,13 @@ function findOutlier(integers){
     }
 }
 
-// console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
-// console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
+// Refactored solution from community best practices
+function findOutlier(integers) {
+    let even = integers.filter(int => int % 2 === 0);
+    let odd = integers.filter(int => int % 2 !== 0);
+    return even.length === 1 ? even[0] : odd[0];
+}
+
+
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
