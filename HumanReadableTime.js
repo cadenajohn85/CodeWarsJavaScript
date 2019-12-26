e6fda4e51f038988892b1dd1c973bc8c45febae6
John Cadena <cadena.john.85@gmail.com>
2019-12-26 08:58:20 -0800

    refactor HumanReadableTime to improve readability

--- HumanReadableTime.js
+++ HumanReadableTime.js
@@ -6,25 +6,44 @@
 // SS = seconds, padded to 2 digits, range: 00 - 59
 // The maximum time never exceeds 359999 (99:59:59)
 
+// Original solution
+
+// function humanReadable(intervalInSeconds) {
+//     const SECONDS_IN_HOUR = 3600;
+//     const SECONDS_IN_MINUTE = 60;
+//
+//     let readableHours = Math.floor(intervalInSeconds / SECONDS_IN_HOUR);
+//     let remainder = intervalInSeconds % SECONDS_IN_HOUR;
+//
+//     let readableMinutes = Math.floor(remainder / SECONDS_IN_MINUTE);
+//     let readableSeconds = remainder % SECONDS_IN_MINUTE;
+//
+//     return formatTimeComponent(readableHours) + ":" + formatTimeComponent(readableMinutes) + ":" + formatTimeComponent(readableSeconds);
+// }
+//
+// function formatTimeComponent(timeComponent) {
+//     if (timeComponent < 10) {
+//         return "0" + timeComponent;
+//     } else {
+//         return timeComponent;
+//     }
+// }
+
+// Refactor based on community samples
+
 function humanReadable(intervalInSeconds) {
     const SECONDS_IN_HOUR = 3600;
     const SECONDS_IN_MINUTE = 60;
 
-    let readableHours = Math.floor(intervalInSeconds / SECONDS_IN_HOUR);
-    let remainder = intervalInSeconds % SECONDS_IN_HOUR;
+    let hours = Math.floor( intervalInSeconds / SECONDS_IN_HOUR ) ;
+    let minutes = Math.floor( intervalInSeconds / SECONDS_IN_MINUTE ) % SECONDS_IN_MINUTE;
+    let seconds = intervalInSeconds % SECONDS_IN_MINUTE;
 
-    let readableMinutes = Math.floor(remainder / SECONDS_IN_MINUTE);
-    let readableSeconds = remainder % SECONDS_IN_MINUTE;
-
-    return formatTimeComponent(readableHours) + ":" + formatTimeComponent(readableMinutes) + ":" + formatTimeComponent(readableSeconds);
+    return pad(hours) + ":" +pad(minutes) + ":" + pad(seconds);
 }
 
-function formatTimeComponent(timeComponent) {
-    if (timeComponent < 10) {
-        return "0" + timeComponent;
-    } else {
-        return timeComponent;
-    }
+function pad(value) {
+    return value < 10 ? "0" + value : value;
 }
 
-console.log(humanReadable(86399));
+console.log(humanReadable(38699));
